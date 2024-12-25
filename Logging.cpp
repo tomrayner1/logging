fdb3e8f405cc741b19506844890a06049f1a4d69
Tom Rayner <52078213+tomrayner1@users.noreply.github.com>
2024-12-25 18:30:59 +0000

    start log func

--- Logging.cpp
+++ Logging.cpp
@@ -2,26 +2,6 @@
 
 namespace logging {
 
-  template <typename... Args>
-  void Info(const LoggingTemplate& temp, const Args&... args) {
-    
-  }
-
-  template <typename... Args>
-  void Warn(const LoggingTemplate& temp, const Args&... args) {
-
-  }
-
-  template <typename... Args>
-  void Error(const LoggingTemplate& temp, const Args&... args) {
-
-  }
-
-  template <typename... Args>
-  void Debug(const LoggingTemplate& temp, const Args&... args) {
-
-  }
-
   namespace internal {
     void concatenate(std::ostringstream& oss) {}
 
@@ -38,5 +18,30 @@ namespace logging {
       logging::internal::concatenate(oss, args...);
     }
   } // namespace logging::internal
+
+  template <typename... Args>
+  void Info(const LoggingTemplate& temp, const Args&... args) {
+    std::ostringstream oss;
+    internal::concatenate(oss, args...);
+
+    std::string log = oss.str();
+
+    std::cout << log << '\n';
+  }
+
+  template <typename... Args>
+  void Warn(const LoggingTemplate& temp, const Args&... args) {
+
+  }
+
+  template <typename... Args>
+  void Error(const LoggingTemplate& temp, const Args&... args) {
+
+  }
+
+  template <typename... Args>
+  void Debug(const LoggingTemplate& temp, const Args&... args) {
+
+  }
 
 } // namespace logging
--- tests/TestMain.cpp
+++ tests/TestMain.cpp
@@ -1,3 +1,8 @@
+
+#include "Logging.h"
+
 int main() {
+  logging::Info(logging::globals::g_DefaultLoggingTemplate, "Testing");
+
   return 0;
 }
